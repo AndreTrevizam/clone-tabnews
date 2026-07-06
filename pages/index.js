19151cb71d73157ca96f4663b0ab8cd7209c3204
Andre Trevizam <andretrevizam264@gmail.com>
2026-07-06 11:54:14 -0300

    adciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 export function Home() {
-  return <h1>Eu amo a Isa, ela é a melhor namorada do mundo !!! ❤️</h1>
+  return <h1>Eu amo a Isa, ela é a melhor namorada do mundo !!! ❤️</h1>;
 }
 
 export default Home;
